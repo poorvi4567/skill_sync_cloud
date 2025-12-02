013b088c70ad42b4d3ec8b1529eda84376c0bc9c
Aarushtube1 <aarushjaiswal.0712@gmail.com>
2025-12-02 15:59:32 +0530

    Matching algo (one hot vector and cosine similarity)

---
+++ app/ml/matcher.py
@@ -0,0 +1,14 @@
+# app/ml/matcher.py
+
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+def build_skill_vector(user_skills, all_skills):
+    vec = np.zeros(len(all_skills))
+    for s in user_skills:
+        idx = all_skills.index(s)
+        vec[idx] = 1
+    return vec
+
+def compute_match_score(mentor_vec, mentee_vec):
+    return cosine_similarity([mentor_vec], [mentee_vec])[0][0]
